babf192f1f8a21ad8d540bd073f96bf07426062e
Frenchfan <sumkin.gpb@gmail.com>
2021-12-02 20:07:15 +0300

    2. Сокобан. Тестовое задание. [#75283 #234679]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,40 +3,35 @@ package ru.job4j.puzzle;
 public class Win {
 
     public static boolean check(int[][] board) {
-        return (checkHorizontal(board) || checkVertical(board));
-    }
-
-    public static boolean checkVertical(int[][] board) {
-        boolean result = true;
-        for (int column = 0; column < board.length; column++) {
-            result = true;
-            for (int row = 0; row < board[column].length; row++) {
-                if (board[row][column] != 1) {
-                    result = false;
-                    break;
-                }
-            }
-            if (result) {
-                return result;
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (checkHorizontal(board, i) || checkVertical(board, i))) {
+                result = true;
+                break;
             }
         }
         return result;
     }
 
-    public static boolean checkHorizontal(int[][] board) {
-        boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            result = true;
-            for (int column = 0; column < board[row].length; column++) {
-                if (board[row][column] != 1) {
-                    result = false;
-                    break;
-                }
-            }
-            if (result) {
-                return result;
-            }
-        }
+    public static boolean checkVertical(int[][] board, int column) {
+         boolean result = true;
+         for (int row = 0; row < board[column].length; row++) {
+             if (board[row][column] != 1) {
+                 result = false;
+                 break;
+             }
+         }
+        return result;
+    }
+
+    public static boolean checkHorizontal(int[][] board, int row) {
+         boolean result = true;
+         for (int column = 0; column < board[row].length; column++) {
+             if (board[row][column] != 1) {
+                 result = false;
+                 break;
+             }
+         }
         return result;
     }
 }
